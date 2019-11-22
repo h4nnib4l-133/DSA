999839f6d57d17ac7ead7c420028dfb1bee286a5
internust133 <140476@ust-global.com>
2019-11-22 17:52:51 +0530

    ll

---
+++ linked-lists-gfg/insert.cpp
@@ -0,0 +1,36 @@
+#include<iostream> 
+
+using namespace std;
+
+class Node {
+	public: 
+	int data;
+	struct Node* next;	
+};
+
+class ll {
+	private:
+
+	public:
+		ll::createll{
+				
+		
+		}	
+
+
+
+};
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+}
+
